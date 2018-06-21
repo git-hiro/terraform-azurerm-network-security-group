e6ccf5a21c4fc9006aad82614c740a72678ecc21
git-hiro <gmai1.hiro@gmail.com>
2018-06-21 20:28:45 +0900

    update

--- output.tf
+++ output.tf
@@ -2,7 +2,6 @@ output "nsg" {
   value = "${
     map(
       "resource_group_name", "${azurerm_network_security_group.nsg.resource_group_name}",
-      "id", "${azurerm_network_security_group.nsg.id}",
       "name", "${azurerm_network_security_group.nsg.name}",
     )
   }"
